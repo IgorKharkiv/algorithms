0130f4fd8aa120d269b69c236130b2b6a06f70c9
Ihor Syrov <syrov.igor1988@gmail.com>
2019-11-20 16:42:45 +0200

    Graph. addEdge, removeEdge, removeVertex

--- src/app/classes/graph.class.ts
+++ src/app/classes/graph.class.ts
@@ -1,8 +1,33 @@
 export class Graph {
-  public adjacencyList: object = {};
+  public adjacencyList: { [key: string]: string[] } = {};
   public addVertex(key: string): void {
-    if (this.adjacencyList[key] === undefined) {
+    if (key && this.adjacencyList[key] === undefined) {
       this.adjacencyList[key] = [];
+    }
+  }
+
+  public addEdge(vertexA: string, vertexB: string): void {
+    if (this.adjacencyList[vertexA] && this.adjacencyList[vertexB]) {
+      if (!this.adjacencyList[vertexA].includes(vertexB)) {
+        this.adjacencyList[vertexA].push(vertexB);
+        this.adjacencyList[vertexB].push(vertexA);
+      }
+    }
+  }
+
+  public removeEdge(vertexA: string, vertexB: string): void {
+    if (this.adjacencyList[vertexA] && this.adjacencyList[vertexB]) {
+      this.adjacencyList[vertexA] = this.adjacencyList[vertexA].filter(v => v !== vertexB);
+      this.adjacencyList[vertexB] = this.adjacencyList[vertexB].filter(v => v !== vertexA);
+    }
+  }
+
+  public removeVertex(vertex: string): void {
+    if (vertex && this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex].forEach(ver => {
+        this.adjacencyList[ver] = this.adjacencyList[ver].filter(v => v !== vertex);
+      });
+      delete this.adjacencyList[vertex];
     }
   }
 }
--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -8,6 +8,8 @@ import { Graph } from '../../classes/';
 })
 export class GraphComponent implements OnInit {
   @ViewChild('vertexInput', null) public vertexInput: ElementRef<HTMLInputElement>;
+  @ViewChild('vertexAInput', null) public vertexAInput: ElementRef<HTMLInputElement>;
+  @ViewChild('vertexBInput', null) public vertexBInput: ElementRef<HTMLInputElement>;
   public graph: Graph;
 
   ngOnInit() {
@@ -17,6 +19,26 @@ export class GraphComponent implements OnInit {
   public addVertex(): void {
     const key = this.vertexInput.nativeElement.value;
     this.graph.addVertex(key);
-    console.log(this.graph);
+    console.log(this.graph.adjacencyList);
+  }
+
+  public addEdge(): void {
+    const verA = this.vertexAInput.nativeElement.value;
+    const verB = this.vertexBInput.nativeElement.value;
+    this.graph.addEdge(verA, verB);
+    console.log(this.graph.adjacencyList);
+  }
+
+  public removeEdge(): void {
+    const verA = this.vertexAInput.nativeElement.value;
+    const verB = this.vertexBInput.nativeElement.value;
+    this.graph.removeEdge(verA, verB);
+    console.log(this.graph.adjacencyList);
+  }
+
+  public removeVertex(): void {
+    const ver = this.vertexAInput.nativeElement.value;
+    this.graph.removeVertex(ver);
+    console.log(this.graph.adjacencyList);
   }
 }
